37db1895e5018ff202d1b65f6381440fb24cbeab
tianbo xu <798209905@qq.com>
2022-08-22 21:56:01 +0800

    上传第一天作业

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,8 +3,10 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author rickxu
@@ -17,13 +19,11 @@ import java.util.List;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List getBotInfo(){
-        List<String> list = new LinkedList<>();
+    public Map<String, String>  getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "15000");
 
-        list.add("sword");
-        list.add("app");
-        list.add("cat");
-
-        return list;
+        return bot1;
     }
 }
