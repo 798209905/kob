9dfd3fbbb3342a561772d77bb1e706f3574851df
tianbo xu <798209905@qq.com>
2022-08-24 11:40:27 +0800

    创建地图和障碍物

---
+++ web/src/views/ranklist/RanklistIndexView.vue
@@ -0,0 +1,21 @@
+<template>
+    <ContentField>
+        排行榜
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../components/ContentField.vue'
+
+export default{
+    components:{
+        ContentField
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
